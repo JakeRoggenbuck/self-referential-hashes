30d68b5e078acad530490524b608136bfa3ed635
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-05-15 00:08:15 -0700

    Add all

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::thread;
 fn run(number: i32, before_string: String) {
     let mut file = File::create(format!("{}_output.txt", number)).unwrap();
     for i in 0..i64::MAX {
-
         let mut hasher = Md5::new();
         let a = format!("{}{:x}", before_string, i);
 
@@ -27,9 +26,12 @@ fn run(number: i32, before_string: String) {
         }
 
         if found {
-            println!("MD5({}) = {:x}, and {} hashes checked for thread {}", a, result, i, number);
+            println!(
+                "MD5({}) = {:x}, and {} hashes checked for thread {}",
+                a, result, i, number
+            );
             let d: &str = &format!("MD5({}) = {:x}\n", a, result);
-            file.write(d.as_bytes());
+            let _ = file.write(d.as_bytes());
         }
     }
 }
@@ -38,14 +40,16 @@ fn main() -> std::io::Result<()> {
     let mut handles = vec![];
 
     let strings = vec![
-        "Jake's MD5 hash: ",
-        "Jake's MD5 hash = ",
-        "Jake's hash: ",
-        "Jake's hash = ",
-        "Jake's self-referential MD5 hash: ",
-        "Jake's self-referential MD5 hash = ",
-        "Jake's self-referential hash: ",
-        "Jake's self-referential hash = ",
+        "This text made by Jake has an MD5 hash starting with ",
+        "Jake crafted this message so its MD5 hash starts with ",
+        "This sentence, written by Jake, has an MD5 hash beginning with ",
+        "The MD5 hash of this Jake-authored text begins with ",
+        "Jake composed this string to produce an MD5 hash that starts with ",
+        "This string, designed by Jake, yields an MD5 hash starting with ",
+        "Written by Jake, this message’s MD5 hash begins with ",
+        "Jake’s text was engineered to start its MD5 hash with ",
+        "This is a message from Jake whose MD5 hash starts with ",
+        "Jake made this message so that its MD5 hash would begin with ",
     ];
 
     for i in 0..7 {
