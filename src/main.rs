c415b5fc746eccbe54513d086cfa88ecb8a2c4ca
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-05-21 11:25:11 -0700

    Add smaller version

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,6 @@
-use md5::{Md5, Digest};
-use std::fs::File;
-use std::io::prelude::*;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::time::Instant;
-use std::sync::Arc;
-use rayon::prelude::*;
-use std::collections::VecDeque;
-use std::sync::Mutex;
+fn main() {
+    let start: i64 = 4_000_000;
+    let end: i64 = 6_000_000_000;
 
-const BATCH_SIZE: usize = 1000;
-const MAX_DIGITS: usize = 16;
-
-fn run(thread_id: i32) {
-    loop {
-        let start = 16i64.pow(8 as u32 - 1);
-        let end = 16i64.pow(9 as u32);
-
-        let total_numbers = end - start;
-
-        let chunks: Vec<_> = (start..end)
-            .collect::<Vec<_>>()
-            .chunks(BATCH_SIZE)
-            .map(|chunk| {
-                let chunk_start = *chunk.first().unwrap();
-                let chunk_end = *chunk.last().unwrap() + 1;
-                (chunk_start, chunk_end)
-            })
-            .collect();
-    }
-}
-
-fn main() -> std::io::Result<()> {
-    let num_threads = num_cpus::get();
-
-    let handles: Vec<_> = (0..num_threads)
-        .map(|i| {
-            std::thread::spawn(move || {
-                run(i as i32);
-            })
-        })
-        .collect();
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    Ok(())
+    let binding = (start..end).collect::<Vec<_>>();
 }
