2f376027c981b37681d3b64ab82305c71d794bdf
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-05-21 10:40:09 -0700

    Remove progress

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,11 @@ use std::sync::Mutex;
 const BATCH_SIZE: usize = 1000;
 const MAX_DIGITS: usize = 16;
 
-struct Progress {
-    current_digit: AtomicUsize,
-}
-
-impl Progress {
-    fn new() -> Self {
-        Self {
-            current_digit: AtomicUsize::new(1),
-        }
-    }
-}
-
-fn run(thread_id: i32, progress: Arc<Progress>) {
+fn run(thread_id: i32) {
     loop {
-        let digit_length = progress.current_digit.fetch_add(1, Ordering::SeqCst);
-        if digit_length > MAX_DIGITS {
-            break;
-        }
+        let start = 16i64.pow(8 as u32 - 1);
+        let end = 16i64.pow(9 as u32);
 
-        let start = if digit_length == 1 { 0 } else { 16i64.pow(digit_length as u32 - 1) };
-        let end = 16i64.pow(digit_length as u32);
         let total_numbers = end - start;
 
         let chunks: Vec<_> = (start..end)
@@ -47,14 +31,12 @@ fn run(thread_id: i32, progress: Arc<Progress>) {
 }
 
 fn main() -> std::io::Result<()> {
-    let progress = Arc::new(Progress::new());
     let num_threads = num_cpus::get();
 
     let handles: Vec<_> = (0..num_threads)
         .map(|i| {
-            let progress = Arc::clone(&progress);
             std::thread::spawn(move || {
-                run(i as i32, progress);
+                run(i as i32);
             })
         })
         .collect();
